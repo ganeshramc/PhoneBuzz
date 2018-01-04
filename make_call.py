9d593874958fd586be30326ee8d303574c823b02
Ganesh <ganesh@tourupapp.com>
2018-01-04 13:00:54 -0800

    Implemented voiceresponse

--- make_call.py
+++ make_call.py
@@ -18,10 +18,9 @@ class MakeCalls:
     @staticmethod
     def play_game():
         response = VoiceResponse()
-        # gather = Gather(action='/process_gather.php', method='GET')
-        # gather.say("Please enter number followed by pound")
-        # response.append(gather)
-        response.say("Hello")
+        gather = Gather(action='/process_gather.php', method='GET')
+        gather.say("Please enter number followed by pound")
+        response.say(gather)
         return response
 
     @staticmethod
