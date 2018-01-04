144ea1f6854505dddce507bb741d0215891fe7e8
Ganesh <ganesh@tourupapp.com>
2018-01-04 07:49:59 -0800

    Added application.py

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -6,3 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Dummy!!!!!!'
+
+
+@app.route('/hello1')
+def print_hello():
+    return 'Helloooookadhfoiae'
+
+@app.route('/hello1/<username>')
+def print_hello1(username):
+    return 'Helle' + username
+
+
+@app.route('/hello/<username>')
+def yolo(username):
+    return 'hellooooo '+ username
