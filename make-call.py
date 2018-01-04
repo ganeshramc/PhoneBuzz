c32333ef397439b3470bac3c7fcea73db18b2993
Ganesh <ganesh@tourupapp.com>
2018-01-04 08:19:25 -0800

    make it better

---
+++ make-call.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+# put your own credentials here
+account_sid = "AC0dbebc41c0d4125a118b5f5958fc3c81"
+auth_token  = "290df12e2450cc200d9b1df988731e37"
+
+# set up a client to talk to the Twilio REST API
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(to="+19494197775",    # to your cell phone
+                           from_="+14086693946", # from your Twilio phone number
+                           url="https://www.twilio.com/docs/twiml-snippet/quickstart")
+
+print(call.sid)
