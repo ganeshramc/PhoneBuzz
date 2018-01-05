d93eafb191d7f4180830ccc851ae4e6b373525b2
Ganesh <ganesh@tourupapp.com>
2018-01-05 01:03:19 -0800

    Added reverify

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 import os
 
-account_sid = os.environ("TWILIO_ACCOUNT_SID")  # replace this with your twilio account_sid
-auth_token  = os.environ("TWILIO_AUTH_TOKEN")  # replace this with your twilio auth token
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID", None)  # replace this with your twilio account_sid
+auth_token  = os.environ.get("TWILIO_AUTH_TOKEN", None)  # replace this with your twilio auth token
